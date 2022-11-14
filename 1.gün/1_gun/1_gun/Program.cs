73fa85ba01f38618cfc0496c96e29012d2df56af
Nurihan_Yılmaz <ynurihan1@gmail.com>
2022-11-14 16:03:38 +0300

    Sayının tek mi çift mi olduğunu bulma

---
+++ 1.gün/1_gun/1_gun/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-tdmplate for more information
+int sayi;
+Console.Write("sayi :");
+sayi = Convert.ToInt32(Console.ReadLine());
+if (sayi%2==0)
+{
+    Console.WriteLine("sayı çift");
+}
+else {
+    Console.WriteLine("sayı tek");
+     }
+
+
+
+
+Console.ReadKey();
